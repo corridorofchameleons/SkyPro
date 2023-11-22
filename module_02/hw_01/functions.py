dbc0e8b0b9ed843d07a1d9296cfb080453d3f557
speedfreak <gdramze@gmail.com>
2023-11-22 22:04:24 +0300

    add module 2 hw_01

---
+++ module_02/hw_01/functions.py
@@ -0,0 +1,74 @@
+from random import shuffle
+
+# настройки программы
+WORDS_FILENAME = 'words.txt'
+RESULTS_FILENAME = 'results_fake_db.txt'
+
+POINTS_PER_ANSWER = 10
+
+
+def shuffle_word(word):
+    '''
+    Принимает слово, возвращает анаграмму
+    :param word:
+    :return:
+    '''
+
+    tmp_word = list(word)
+    shuffle(tmp_word)
+    return ''.join(tmp_word)
+
+
+def read_words(filename=WORDS_FILENAME):
+    '''
+    Читает слова из файла, возвращает список слов
+    :param filename:
+    :return:
+    '''
+
+    try:
+        with open(filename, 'r') as file:
+            lines = [line.strip() for line in file.readlines()]
+            return lines
+    except:
+        print('Ошибка чтения файла')
+        return
+
+
+def write_results(name, res, filename=RESULTS_FILENAME):
+    '''
+    Сохраняет результат
+    :param filename:
+    :param name:
+    :param res:
+    :return:
+    '''
+
+    try:
+        with open(filename, 'a') as file:
+                line = ':'.join([name, str(res)]) + '\n'
+                file.writelines(line)
+    except:
+        print('Результат не удалось сохранить')
+
+
+def read_results(filename=RESULTS_FILENAME):
+    '''
+    Читает результаты
+    :param filename:
+    :return:
+    '''
+
+    try:
+        with open(filename, 'r') as file:
+            if file:
+                data = file.readlines()
+                records = [int(rec.split(':')[1].strip()) for rec in data]
+
+                # количество игр и рекорд
+                games_num = len(records) if records else 0
+                top_record = max(records) if records else 0
+
+                return games_num, top_record
+    except:
+        print('Не удалось прочитать результаты')
---
+++ module_02/hw_01/main.py
@@ -0,0 +1,52 @@
+from functions import *
+
+
+def main():
+
+    # создаем счетчик очков
+    result = 0
+
+    # результаты игры
+    games_num = 0
+    record = 0
+
+    # получает слова
+    words = read_words()
+
+    if words:
+
+        username = input('Введите ваше имя:\n')
+
+        for word in words:
+            # визуально разделяет раунды
+            print()
+
+            shuffled_word = shuffle_word(word)
+            answer = input(f'Угадайте слово: {shuffled_word}\n')
+
+            if answer.strip().lower() == word:
+                print(f'Верно, вы получаете {POINTS_PER_ANSWER} очков')
+                result += POINTS_PER_ANSWER
+            else:
+                print(f'Неверно! Верный ответ – {word}')
+
+        # сохраняет результат
+        write_results(username, result)
+
+        # получает статистику
+        results = read_results()
+
+        if results:
+            games_num, record = read_results()
+
+        # выводит статистику
+        print()
+        print(f'Всего игр сыграно: {games_num}')
+        print(f'Лучший результат: {record}')
+
+    else:
+        print('Игра аварийно завершилась')
+
+
+if __name__ == '__main__':
+    main()
